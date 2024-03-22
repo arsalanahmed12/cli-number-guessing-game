92f481bd530d4139826624c8344b7b4722e09ce4
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-23 02:27:08 +0500

    creat a number game

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { name: "guess",
+        type: "number",
+        message: "Guess a number between 1-10 and win the game" }
+]);
+if (answer.guess === randomnumber) {
+    console.log("Congratulations! You Win The Game");
+}
+else {
+    console.log(`Try Again Correct Number Is ${randomnumber}`);
+}
